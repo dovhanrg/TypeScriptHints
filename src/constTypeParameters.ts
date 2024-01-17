d1d94aefb421724ac788c220423a964224bec344
romandovhan <roman.dovhan@incredee.com>
2024-01-17 17:26:39 +0200

    constTypeParameters.ts

--- src/constTypeParameters.ts
+++ src/constTypeParameters.ts
@@ -4,9 +4,11 @@
 declare function useStatuses<T>(statuses: T[]): T;
 
 const loadingStatus = useStatuses(['loading', 'idle']);
+// const loadingStatus: string
 
 // with const T
 
 declare function useStatuses2<const T>(statuses: T[]): T;
 
 const loadingStatus2 = useStatuses2(['loading', 'idle']);
+// const loadingStatus2: "loading" | "idle"
